cafc4ba2abac112d4e1f8f0b5e28b1b55df247ed
Saba Shavidze <sabashavidze@gmail.com>
2023-06-28 13:22:56 +0400

    use curry to transform normal functions to curried

--- curry.js
+++ curry.js
@@ -8,3 +8,23 @@ const replaceVowels = replace(/[AEIOU]/gi, "!");
 const result = replaceVowels("Hey Bro, I have words where are vowels");
 
 console.log(result);
+
+//
+
+const split = curry((delimiter, string) => string.split(delimiter));
+
+//exercies
+
+const words = split(" ");
+
+console.log(words("xs, as,"));
+
+//
+
+const slice = curry((start, end, xs) => xs.slice(start, end));
+
+console.log(slice(0)(2)(["a", "b", "c"]));
+
+const take = slice(0);
+
+console.log(take(4)(["a", "b", "c", "d", "e", "f", "g", "h"]));
