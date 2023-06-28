1be660c5c685c7b10907abc2f9fb00a3133155fc
Saba Shavidze <sabashavidze@gmail.com>
2023-06-28 16:06:44 +0400

    Functional Programming: Fundamentals

--- src/functor.js
+++ src/functor.js
@@ -26,4 +26,54 @@ const nextCharForNumberString = (str) =>
     .map((number) => new Number(number + 1))
     .fold(String.fromCharCode);
 
-console.log(nextCharForNumberString(" 64 "));
+//console.log(nextCharForNumberString(" 64 "));
+
+// Second example
+
+const first = (xs) => xs[0];
+const answer = (ans) => `The answer is ${ans}`;
+const halfTheFirstLargeNumber = (xs) =>
+  Box(xs)
+    .map((xs) => xs.filter((x) => (x) => 20))
+    .map((x) => first(x) / 2)
+    .fold(answer);
+
+//console.log(halfTheFirstLargeNumber([1, 3, 50]));
+
+// Composition
+
+const compostion = (f, g) => (x) => Box(x).map(g).fold(f);
+
+// Example, moving to Monads
+// applyDiscount
+const percentToFloat = (str) =>
+  Box(str)
+    .map((s) => s.replace(/\$/, ""))
+    .map((s) => parseFloat(s))
+    .fold((f) => f * 0.01);
+const moneyToFloat = (str) =>
+  Box(str)
+    .map((s) => s.replace(/\$/, ""))
+    .fold((s) => parseFloat(s));
+
+/**
+ *
+ * @param {string} price
+ * @param {string} discount
+ * @returns number
+ *
+ * Example: `applyDiscount_('$5.00','20%') => 4`
+ */
+const applyDiscount_ = (price, discount) => {
+  const cents = moneyToFloat(price);
+  const savings = percentToFloat(discount);
+  return cents - cents * savings;
+};
+const applyDiscount = (price, discount) =>
+  Box(moneyToFloat(price)).map((cents) =>
+    Box(percentToFloat(discount)).map(
+      (cents) => cents - cents * percentToFloat(discount)
+    )
+  );
+
+console.log("answer" - applyDiscount("$5.00", "20%"));
