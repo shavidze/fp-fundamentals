4950e3ab68defdd48fe86bcc1bf180adc18e0ce0
Saba Shavidze <sabashavidze@gmail.com>
2023-06-28 13:52:33 +0400

    compose function associative

--- src/composition.js
+++ src/composition.js
@@ -1,4 +1,8 @@
-const add = (x, y) => x + y;
+const { curry } = require("rambda");
+
+const add = curry((x, y) => x + y);
+
+const concat = curry((y, x) => x + y);
 
 const toUpper = (str) => str.toUpperCase();
 
@@ -8,12 +12,17 @@ const first = (xs) => xs[0];
 
 const compose = (f, g) => (x) => f(g(x));
 
-const shout = compose(first, compose(exclaim, toUpper));
+const loud = compose(toUpper, first);
+const shout = compose(loud, exclaim);
 
 //associative
-const shoutAssociative = compose(compose(first, exclaim), toUpper);
+const shoutAssociative = compose(compose(toUpper, first), exclaim);
 
 console.log(shout("tears"));
 console.log(shoutAssociative("tears")); // same
 
-console.log(compose(first, exclaim, toUpper));
+console.log(compose(toUpper, first, exclaim)("tears"));
+
+const shout2 = compose(concat("!"), loud)("tears");
+
+console.log({ shout2 });
