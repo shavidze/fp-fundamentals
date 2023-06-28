408fd1b35eea4fdbf111cd53956cd9e397eb98b5
Saba Shavidze <sabashavidze@gmail.com>
2023-06-28 14:16:24 +0400

    compose function associative

--- src/composition.js
+++ src/composition.js
@@ -15,7 +15,7 @@ const compose = (f, g) => (x) => f(g(x));
 const loud = compose(toUpper, first);
 const shout = compose(loud, exclaim);
 
-//associative
+//associativeness
 const shoutAssociative = compose(compose(toUpper, first), exclaim);
 
 console.log(shout("tears"));
@@ -26,3 +26,9 @@ console.log(compose(toUpper, first, exclaim)("tears"));
 const shout2 = compose(concat("!"), loud)("tears");
 
 console.log({ shout2 });
+
+// logger
+// (console.log(x),x) => ეს სინტაქსი ლოგავს და აბრუნებს x;
+//const log = (tag) => (x) => (console.log(tag, x), x);
+
+const log = curry((tag, x) => (console.log(tag, x), x));
