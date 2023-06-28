0cc700b3494d9879b70ae82dd6562166dfac6545
Saba Shavidze <sabashavidze@gmail.com>
2023-06-28 13:37:47 +0400

    sepearete src file from project

---
+++ src/composition.js
@@ -0,0 +1,17 @@
+const add = (x, y) => x + y;
+
+const toUpper = (str) => str.toUpperCase();
+
+const exclaim = (str) => str + "!";
+
+const first = (xs) => xs[0];
+
+const compose = (f, g) => (x) => f(g(x));
+
+const shout = compose(first, compose(exclaim, toUpper));
+
+//associative
+const shoutAssociative = compose(compose(first, exclaim), toUpper);
+
+console.log(shout("tears"));
+console.log(shoutAssociative("tears")); // same
