2333fedad51e86cc12fd388222b0ea72abf1fdcd
Saba Shavidze <sabashavidze@gmail.com>
2023-06-29 14:42:43 +0400

    add scripts for monad

--- src/monad.js
+++ src/monad.js
@@ -1,10 +1,36 @@
-const findColor = (name) =>
-  ({
+const findColor = (name) => {
+  const found = {
     red: "#ff4444",
     blue: "#3b5998",
     yellow: "#fff68f",
-  }[name]);
+  }[name];
+  return found ? Right(found) : Left("dunno");
+};
 
-const res = findColor("red");
+//const res = findColor("redd").toUpperCase();
 
-console.log(res);
+//console.log(res);
+
+const Right = (x) => ({
+  chain: (f) => f(x),
+  map: (f) => Right(f(x)),
+  fold: (f, g) => g(x),
+  toString: `Right(${x})`,
+});
+
+const Left = (x) => ({
+  chain: (f) => f(x),
+  map: (f) => Left(f(x)),
+  fold: (f, g) => f(x),
+  toString: `Left(${x})`,
+});
+
+const res = () =>
+  findColor("red")
+    .map((x) => x.toUpperCase())
+    .fold(
+      () => "no color",
+      (x) => `founded ${x}`
+    );
+
+console.log(res());
